f9b058605fd5e22632d44616cdf4f5c93d15546b
yishoka <63327438+yishoka@users.noreply.github.com>
2020-04-08 15:35:10 +0800

    認識欄位

--- CSharp/Assets/Car.cs
+++ CSharp/Assets/Car.cs
@@ -2,6 +2,8 @@
 
 // 註解：用來說明紀錄等等.. 只會在此腳本內看到
 // 摘要 summary：描述此內容，會在其他腳本內看到 - 提高維護性與擴充性
+// 新增的 C# 放在專案內為藍圖
+// 放在場景物件上為實體物件
 /// <summary>
 /// 定義汽車物件
 /// </summary>
@@ -17,13 +19,44 @@ public class Car : MonoBehaviour
     // 剎車       - 是否開啟     - 布林值 bool - true、false
 
     // 定義欄位語法：
-    // 資料類型 欄位名稱 (指定 值);
-    // 預設值
+    // 修飾詞 資料類型 欄位名稱 (指定 值);
+
+    // 修飾詞：
+    // 公開 public：允許其他類別存取、顯示在屬性面板上（Inpector)上
+    // 私人 private (預設）：不允許、不顯示
+
+    // 預設值：
     // int、float 為 0
     // string 為 ""
     // bool 為 false
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake = false;
+    // 欄位屬性 Attribute：Unity 提供用於輔助欄位的功能
+    // 語法：[屬性名稱(值)]
+    // 標題 [Header("字串")]
+    // 提示 [Tooltip("字串")]
+    // 範圍 [Range(最小值，最大值)] --僅限於數值類型資料 int、float
+
+    /// <summary>
+    /// 汽車的 CC 數
+    /// </summary>
+    [Header("汽車的 CC 數"), Range(500, 5000)]
+    public int cc = 2000;
+    /// <summary>
+    /// 汽車重量
+    /// </summary>
+    [Header("汽車重量")]
+    [Tooltip("這個欄位是用來存放汽車的噸位")]
+    [Range(50, 350)]
+    public float weight = 100.9f;
+    /// <summary>
+    /// 品牌
+    /// </summary>
+    [Header("品牌")]
+    [Tooltip("代表這台車子的品牌")]
+    public string brand = "BMW";
+    /// <summary>
+    /// 是否剎車
+    /// </summary>
+    [Header("是否剎車"), Tooltip("用來儲存這台車子目前是否為剎車狀態")]
+    public bool brake = false;
 }
+
--- CSharp/Assets/Class1.cs
+++ CSharp/Assets/Class1.cs
@@ -9,7 +9,12 @@ public class Class1 : MonoBehaviour
     // 定義類別成員
     // C #
     // 1. 大小不一樣
-    // 2. 括號成對出現 () [] {} '' "" 
+    // 2. 括號成對出現 () [] {} '' "" < >
+
+    [Header("汽車 1 號")]
+    public Car car1;
+    [Header("汽車 2 號")]
+    public Car car2;
 
     //事件：在特定時間會已指定次數執行的方法 (名稱是藍色)
     //開始事件：播放遊戲時執行一次 - 初始設定
@@ -18,5 +23,15 @@ public class Class1 : MonoBehaviour
         // 輸出("文字訊息");
         // C# 結尾為分號 ;
         print("哈囉，沃德~");
+
+        // 取得 get
+        print("汽車 1 號的 CC數：" + car1.cc);
+        print("汽車 2 號的品牌：" + car2.brand);
+
+        // 設定 set
+        car1.cc = 9999;
+        car1.weight = 999.9f;
+        car1.brand = "TOYOTA";
+        car1.brake = true;
     }
 }
